5ef032da056018bfa9193dd8bf3d5de2cb62fc9a
Dustin Barker <barkerd85@yahoo.com>
2021-11-19 10:16:05 -0700

    Added the delete funtion to the music table

---
+++ src/components/MusicTable/MusicTable.jsx
@@ -0,0 +1,47 @@
+import React from 'react'; 
+import axios from 'axios';
+import './MusicTable.css'
+
+
+
+function MusicTable (props){   
+        return ( 
+            <table class="center">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Artist</th>
+                        <th>Album</th>
+                        <th>Genre</th>
+                        <th>Release Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.songs.map(song =>
+                        <tr>
+                            <td>{song.title}</td>
+                            <td>{song.artist}</td>
+                            <td>{song.album}</td>
+                            <td>{song.genre}</td>
+                            <td>{song.release_date}</td>
+                            <button onClick={()=>this.deleteSong(song)}>Delete</button>
+                        </tr>
+                        )}
+                </tbody>
+            </table>
+         );
+
+            
+function deleteSong(){
+    axios.delete('http://127.0.0.1:8000/music/')
+    .then(response => this.setState({
+      library: response.data
+    })
+  )}
+
+
+}
+                            
+
+ 
+export default MusicTable;
